6b83949c5aecb5eb0eff280e16a5b53ce4b5032c
Jin Yan <yanjg@uw.edu>
2018-05-25 04:48:32 -0700

    finish

--- sentiment_analysis/sentiment_analysis/student_analysis.R
+++ sentiment_analysis/sentiment_analysis/student_analysis.R
@@ -1,14 +1,16 @@
 # Student sentence analysis - look at sentiments of students' sentences
 
-# install.packages('syuzhet')
 library(syuzhet)
 
 # Create a vector of emotional sentences. 
 # Add some happy ones, angry ones - you name it!
 student_sentences <- c('I really like the pie you gave me this morning.', 
                        'Your shoes suck and are just plain ugly.',
-                       'I\'d really truly love going out in this weather!'
+                       'I\'d really truly love going out in this weather!',
+                       'The weather makes me so hungry!'
                        )
 
 # Analyze sentiment for student sentences
 
+student_sentiments <- get_sentiment(student_sentences, method = 'syuzhet')
+student_analysis <- cbind(centence = student_sentences, student_sentiments)
